4834e4545657326248b6d705858ce181bdbfdf20
alled0 <waleedal305@hotmail.com>
2024-12-01 20:42:24 +0300

    Initial commit

--- config/db.js
+++ config/db.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
 dotenv.config();
-MONGODB_URI="mongodb+srv://alledcss:Ww1234@cluster0.oc4ph.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+
 const connectDB = async () => {
   try {
     await mongoose.connect(MONGODB_URI, {
