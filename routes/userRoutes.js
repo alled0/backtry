ce5953a8537586209eea1be1cdab3ef8e94c54e0
Omar <145347533+OmarAlkhulaif@users.noreply.github.com>
2024-12-11 03:59:20 +0300

    fix

---
+++ routes/userRoutes.js
@@ -0,0 +1,69 @@
+const express = require("express");
+const {
+  signUp,
+  updateProfile,
+  fetchProfile,
+  adminRoute,
+  clubRoute,
+  followClub,
+  unfollowClub,
+  joinEvent,
+  leaveEvent,
+  getEnrolledClubs,
+  getFollowedClubs,
+  getJoinedEvents,
+  fetchProfilebyId,
+  getUserWithReservations,
+} = require("../Controllers/userController");
+const authenticateToken = require("../middleware/authenticateToken");
+const authorizeRoles = require("../middleware/authorizeRoles");
+
+const router = express.Router();
+
+// Sign-Up route
+router.post("/sign-Up", signUp);
+
+// Update Profile route
+router.put("/profile", authenticateToken, updateProfile);
+
+// Fetch Profile route
+router.get("/profile", authenticateToken, fetchProfile);
+
+router.get("/profile/:id", fetchProfilebyId); //fetchProfilebyId
+
+// Admin-only route
+router.get(
+  "/admin-only",
+  authenticateToken,
+  authorizeRoles("admin"),
+  adminRoute
+);
+
+// Club-only route
+router.get(
+  "/club-only",
+  authenticateToken,
+  authorizeRoles("clubAccount"),
+  clubRoute
+);
+
+// Follow/Unfollow Club
+router.post("/follow-club", authenticateToken, followClub);
+router.post("/unfollow-club", authenticateToken, unfollowClub);
+
+// Join/Leave Event
+router.post("/join-event", authenticateToken, joinEvent);
+router.post("/leave-event", authenticateToken, leaveEvent);
+
+// Get Enrolled Clubs
+router.get("/enrolled-clubs", authenticateToken, getEnrolledClubs);
+
+// Get Followed Clubs
+router.get("/followed-clubs", authenticateToken, getFollowedClubs);
+
+// Get Joined Events
+router.get("/joined-events", authenticateToken, getJoinedEvents);
+
+router.get("/profile/reservations", authenticateToken, getUserWithReservations);
+
+module.exports = router;
--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const Reservation = require("./models/Reservation"); // Update the path if neces
 
 // Routes
 const authRoutes = require("./routes/authRoutes.js");
-const userRoutes = require("./routes/userRoutes.js"); 
+const userRoutes = require("./routes/userRoutes.js");
 const reservationRoute = require("./routes/reservationRoute.js");
 const eventRoutes = require("./routes/eventRoute.js");
 const clubRoutes = require("./routes/clubRoute.js");
@@ -31,7 +31,6 @@ app.use("/api/reservationRoute", reservationRoute);
 app.use("/api/otpRoutes", otpRoutes);
 
 const PORT = process.env.PORT || 4000;
-
 
 try {
   connection();
